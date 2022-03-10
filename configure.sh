35d0d9ed325b81713d36ab3cc32f64738ccca2b2
情缘 <18437931+qingyuan0o0@users.noreply.github.com>
2022-03-10 16:13:22 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mkdir ~/.screen && chmod 700 ~/.screen
 export SCREENDIR=$HOME/.screen
-rssh -l :2222 &
-/usr/bin/ttyd -p $PORT -c $US_ER:$PA_SS bash
+/usr/bin/ttyd -p $PORT -c $US_ER:$PA_SS -u 1000 bash
