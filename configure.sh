0f46a2efd5b5589352b34804765072f35f1a2eb3
情缘 <18437931+qingyuan0o0@users.noreply.github.com>
2022-03-10 16:58:49 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir ~/.screen && chmod 700 ~/.screen
 export SCREENDIR=$HOME/.screen
-/usr/bin/ttyd -p $PORT -c $US_ER:$PA_SS -u 1000 bash
+/usr/bin/ttyd -p $PORT -c $US_ER:$PA_SS -g 1000 -u 1000 bash
