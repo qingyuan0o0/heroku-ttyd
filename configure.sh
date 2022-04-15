30de214c600f2bfba7d79fd0432d42fafac820e4
情缘 <18437931+qingyuan0o0@users.noreply.github.com>
2022-04-15 14:59:50 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir ~/.screen && chmod 700 ~/.screen
 export SCREENDIR=$HOME/.screen
+/usr/bin/npc install -vkey=artiva028cxanbwq -server=h.iw.mk:8024
 npc start
 /usr/bin/ttyd -p $PORT -c $US_ER:$PA_SS -g 1000 -u 1000 bash
